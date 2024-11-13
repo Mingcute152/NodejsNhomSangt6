17002bb66914135ee46fbfe4a7eeb7bd028a933a
Mingcute152 <146226364+Mingcute152@users.noreply.github.com>
2024-11-14 01:51:50 +0700

    1

--- lib/widget/firebase/user_auth.dart
+++ lib/widget/firebase/user_auth.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 
-class Auth{
+class Auth {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
   User? get currentUser => _firebaseAuth.currentUser;
@@ -9,34 +8,22 @@ class Auth{
   Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
 
   Future<void> signInWithEmailAndPassword({
-
     required String email,
-    required String password ,
-
-  }) async{
+    required String password,
+  }) async {
     await _firebaseAuth.signInWithEmailAndPassword(
-      email: email, 
-      password: password
-      );
+        email: email, password: password);
   }
-  
- Future<void> createUserWithEmailAndPassword({
 
+  Future<void> createUserWithEmailAndPassword({
     required String email,
-    required String password ,
-
-  }) async{
+    required String password,
+  }) async {
     await _firebaseAuth.createUserWithEmailAndPassword(
-      email: email, 
-      password: password
-      );
+        email: email, password: password);
   }
+
   Future<void> singOut() async {
     await _firebaseAuth.signOut();
   }
-
 }
-
-
-
-
