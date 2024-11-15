83ecf68f552b109df39de49ea2a25f62d8609bb7
Mingcute152 <146226364+Mingcute152@users.noreply.github.com>
2024-11-16 01:13:22 +0700

    update cart

--- lib/widget/products_cart.dart
+++ lib/widget/products_cart.dart
@@ -15,31 +15,64 @@ class ProductsCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 120,
-      padding: const EdgeInsets.all(10),
-      margin: const EdgeInsets.only(right: 15),
-      decoration: BoxDecoration(
-        color: whiteColor,
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: [
-          BoxShadow(
-            // color: grey35Color.withOpacity(5.0),
-            spreadRadius: 1,
-            blurRadius: 1,
-            offset: const Offset(0, 0),
-          )
-        ],
-      ),
-      child: Column(
-        children: [
-          Image.asset(icon, width: 70, height: 90),
-          Text(
-            products_information,
-            style: TextStyle(
-                color: blackColor, fontSize: 15, fontWeight: FontWeight.bold),
-          ),
-        ],
-      ),
-    );
+        width: 150,
+        padding: const EdgeInsets.all(10),
+        margin: const EdgeInsets.only(right: 15),
+        decoration: BoxDecoration(
+          color: whiteColor,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              // color: grey35Color.withOpacity(5.0),
+              spreadRadius: 1,
+              blurRadius: 1,
+              offset: const Offset(0, 0),
+            )
+          ],
+        ),
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.topLeft,
+              margin: EdgeInsets.fromLTRB(0, 0, 30, 0),
+              decoration: BoxDecoration(
+                color: whiteColor,
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Text(
+                'free dedivery',
+                style: TextStyle(
+                    color: greenColor,
+                    fontSize: 10,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Image.asset(icon, width: 100, height: 70),
+            Text(
+              products_information,
+              style: TextStyle(
+                  color: blackColor, fontSize: 15, fontWeight: FontWeight.bold),
+            ),
+            RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(
+                text: 'hello\n',
+                style: TextStyle(
+                    fontSize: 10,
+                    color: blackColor,
+                    fontWeight: FontWeight.normal),
+                children: <TextSpan>[
+                  TextSpan(
+                    text: 'giá : 1000đ',
+                    style: TextStyle(
+                        fontSize: 10,
+                        color: Colors.red,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
