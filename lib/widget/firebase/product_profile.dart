9bf7c8c2dbf11fd47b54d37c152b167b2029435c
Mingcute152 <146226364+Mingcute152@users.noreply.github.com>
2024-11-22 14:25:43 +0700

    uptate data sp

---
+++ lib/widget/firebase/product_profile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class inforProduct extends StatelessWidget {
+  const inforProduct({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
